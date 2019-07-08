f0ae8c964df1ed84b3a9a6b2d29633daccd681a3
Arjun <14865564+arjunzv8@users.noreply.github.com>
2019-07-08 17:41:00 +1000

    Made scripts for CAN setup and Knee movement

---
+++ BBB Scripts/CAN_add_nodes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/debian/CANopenSocket/canopend
+
+# & at the end is used to run processes in background
+
+#adding nodes
+#node 1
+echo - > od1_storage
+echo - > od1_storage_auto
+app/canopend can1 -i 1 -s od1_storage -a od1_storage_auto &
+
+#node 2
+echo - > od2_storage
+echo - > od2_storage_auto
+app/canopend can1 -i 2 -s od2_storage -a od2_storage_auto &
+
+#master
+app/canopend can1 -i 100 -c ""
