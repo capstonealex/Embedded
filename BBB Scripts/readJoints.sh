051045ce890894483592f6d71e4de3a84281026d
Arjun <14865564+arjunzv8@users.noreply.github.com>
2019-08-30 05:20:49 +1000

    updated read joint to read all joints

--- BBB Scripts/readJoints.sh
+++ BBB Scripts/readJoints.sh
@@ -6,7 +6,11 @@ cd /home/debian/CANopenSocket/canopencomm
 #./canopencomm 2 start
 #./canopencomm 1 start
 
-echo "current knee value"
-./canopencomm [1] 4 read 0x6063 0 i32
-echo "current hip value"
+echo "current Lhip value"
+./canopencomm [1] 1 read 0x6063 0 i32
+echo "current Lknee value"
+./canopencomm [1] 2 read 0x6063 0 i32
+echo "current Rhip value"
 ./canopencomm [1] 3 read 0x6063 0 i32
+echo "current Rknee value"
+./canopencomm [1] 4 read 0x6063 0 i32
