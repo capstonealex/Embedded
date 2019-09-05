f36be443a3933e50fbef061a0e4bff4979ed3ffc
Arjun <14865564+arjunzv8@users.noreply.github.com>
2019-09-05 13:50:45 +1000

    pdo torque set

--- BBB Scripts/Torque PDO.sh
+++ BBB Scripts/Torque PDO.sh
@@ -36,10 +36,10 @@
 ./canopencomm [1] 3 write 0x1802 1 u32 0x383
 ./canopencomm [1] 4 write 0x1802 1 u32 0x384
 
-./canopencomm [1] 100 write 0x1604 1 u32 0x60770110
-./canopencomm [1] 100 write 0x1605 1 u32 0x60770210
-./canopencomm [1] 100 write 0x1606 1 u32 0x60770310
-./canopencomm [1] 100 write 0x1607 1 u32 0x60770410
+./canopencomm [1] 100 write 0x1604 1 u32 0x60410110
+./canopencomm [1] 100 write 0x1605 1 u32 0x60410210
+./canopencomm [1] 100 write 0x1606 1 u32 0x60410310
+./canopencomm [1] 100 write 0x1607 1 u32 0x60410410
 
 ./canopencomm [1] 100 write 0x1604 0 u8 1
 ./canopencomm [1] 100 write 0x1605 0 u8 1
