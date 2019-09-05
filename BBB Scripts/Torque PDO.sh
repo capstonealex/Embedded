6fad64358b6c283244e42a2df6a3022d20145e51
Rob Gadsby <rngadsby@outlook.com>
2019-09-05 12:51:28 +1000

    torque pdo edited

--- BBB Scripts/Torque PDO.sh
+++ BBB Scripts/Torque PDO.sh
@@ -36,10 +36,10 @@
 ./canopencomm [1] 3 write 0x1802 1 u32 0x383
 ./canopencomm [1] 4 write 0x1802 1 u32 0x384
 
-./canopencomm [1] 100 write 0x1604 1 u32 0x60770110
-./canopencomm [1] 100 write 0x1605 1 u32 0x60770210
-./canopencomm [1] 100 write 0x1606 1 u32 0x60770310
-./canopencomm [1] 100 write 0x1607 1 u32 0x60770410
+./canopencomm [1] 100 write 0x1604 1 u32 0x60410110
+./canopencomm [1] 100 write 0x1605 1 u32 0x60410210
+./canopencomm [1] 100 write 0x1606 1 u32 0x60410310
+./canopencomm [1] 100 write 0x1607 1 u32 0x60410410
 
 ./canopencomm [1] 100 write 0x1604 0 u8 1
 ./canopencomm [1] 100 write 0x1605 0 u8 1
@@ -51,4 +51,12 @@
 ./canopencomm [1] 100 write 0x1406 1 u32 0x383
 ./canopencomm [1] 100 write 0x1407 1 u32 0x384
 
+./canopencomm [1] 100 write 0x1404 1 u32 0x80000381
+./canopencomm [1] 100 write 0x1405 1 u32 0x80000382
+./canopencomm [1] 100 write 0x1406 1 u32 0x80000383
+./canopencomm [1] 100 write 0x1407 1 u32 0x80000384
 
+./canopencomm [1] 100 write 0x1604 0 u8 0
+./canopencomm [1] 100 write 0x1605 0 u8 0
+./canopencomm [1] 100 write 0x1606 0 u8 0
+./canopencomm [1] 100 write 0x1607 0 u8 0
