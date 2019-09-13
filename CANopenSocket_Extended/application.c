772fcff6237574801a4d00bbc81142d5117124e7
Arjun <14865564+arjunzv8@users.noreply.github.com>
2019-09-13 15:00:28 +1000

    updated application with torque logger

--- CANopenSocket_Extended/application.c
+++ CANopenSocket_Extended/application.c
@@ -23,116 +23,123 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
-
 #include "CANopen.h"
 #include "stdio.h"
 #include <sys/time.h>
-void itoa(int value, char *str, int base);
-//Reverses a string. Used by itoa()
+//// Data logger helper functions
+void fileLogHeader();
+void fileLogger();
 void strreverse(char *begin, char *end);
+void itoa(int value, char *str, int base);
+/******************************************************************************/
+void app_programStart(void){
+    void fileLogHeader();
+}
+/******************************************************************************/
+void app_communicationReset(void){
+}
+/******************************************************************************/
+void app_programEnd(void){
+}
+/******************************************************************************/
+void app_programAsync(uint16_t timer1msDiff){
+// Timing speed of reading from memory and file writing
+// struct timeval start;
+//  struct timeval stop;
+//    gettimeofday(&start, NULL);
 
+fileLogger();
+
+//  gettimeofday(&stop, NULL);
+//  double elapsed_ms = (stop.tv_sec - start.tv_sec) * 1000.0;
+//  elapsed_ms += (stop.tv_usec - start.tv_usec) / 1000.0;
+//  printf("TASK 1:  %.2f milliseconds\n", elapsed_ms);
+}
+
+/******************************************************************************/
+void app_program1ms(void){
+}
+/******************************************************************************/
 void itoa(int value, char *str, int base)
 {
     static char num[] = "0123456789abcdefghijklmnopqrstuvwxyz";
     char *wstr = str;
     int sign;
-
     // Validate base
     if (base < 2 || base > 35)
     {
         *wstr = '\0';
         return;
     }
-
     // Take care of sign
     if ((sign = value) < 0)
         value = -value;
-
     // Conversion. Number is reversed.
     do
         *wstr++ = num[value % base];
     while (value /= base);
-
     if (sign < 0)
         *wstr++ = '-';
-
     *wstr = '\0';
-
     // Reverse string
     strreverse(str, wstr - 1);
 }
-
 /******************************************************************************/
-
 void strreverse(char *begin, char *end)
 {
     char aux;
     while (end > begin)
         aux = *end, *end-- = *begin, *begin++ = aux;
 }
-
 /******************************************************************************/
-void app_programStart(void){
-
-}
-
-
-/******************************************************************************/
-void app_communicationReset(void){
-
-}
-
-
-/******************************************************************************/
-void app_programEnd(void){
-
-}
-
-
-/******************************************************************************/
-void app_programAsync(uint16_t timer1msDiff){
-    // Timer code
-    // int n= 0;
-    // double tfgyhj = 0;
-
-    struct timeval start;
-    struct timeval stop;
-    gettimeofday(&start, NULL);
-     FILE* fp;
-    fp = fopen("log.txt", "a");
-
+void fileLogger(){
+    printf("fileLogger beggining\n");
+    FILE* fp;
+    fp = fopen("X2_log.txt", "a");
     // Generate whatever you want logged here, "data" is just an example
-    char data [50];
-    itoa(CO_OD_RAM.actualMotorPositions.motor1, data, 10);
-    // This lines writes the info in "data" to the file pointer specified
-    fputs(data, fp);
-    itoa(CO_OD_RAM.actualMotorPositions.motor2, data, 10);
-    // This lines writes the info in "data" to the file pointer specified
-    fputs(data, fp);
-    itoa(CO_OD_RAM.actualMotorPositions.motor3, data, 10);
-    // This lines writes the info in "data" to the file pointer specified
-    fputs(data, fp);
-    itoa(CO_OD_RAM.actualMotorPositions.motor4, data, 10);
-    // This lines writes the info in "data" to the file pointer specified
-    fputs(data, fp);
+    char position [50];
+    char torque[50];
+    char comma[] = ", ";
+    // Motor 1: Left Hip position and Torque
+    itoa(CO_OD_RAM.actualMotorPositions.motor1, position, 10);
+    itoa(CO_OD_RAM.statusWords.motor1, torque, 10);
+    fputs(position, fp);
+    fputs(comma, fp);
+    fputs(torque, fp);
+    fputs(comma, fp);
+    // Motor 2: Left Knee position and Torque
+    itoa(CO_OD_RAM.actualMotorPositions.motor2, position, 10);
+    itoa(CO_OD_RAM.statusWords.motor2, torque, 10);
+    fputs(position, fp);
+    fputs(comma, fp);
+    fputs(torque, fp);
+    fputs(comma, fp);
+    // Motor 3: Right Hip position and Torque
+    itoa(CO_OD_RAM.actualMotorPositions.motor3, position, 10);
+    itoa(CO_OD_RAM.statusWords.motor3, torque, 10);
+    fputs(position, fp);
+    fputs(comma, fp);
+    fputs(torque, fp);
+    fputs(comma, fp);
+    // Motor 4: Right Knee position and Torque
+    itoa(CO_OD_RAM.actualMotorPositions.motor4, position, 10);
+    itoa(CO_OD_RAM.statusWords.motor4, torque, 10);
+    fputs(position, fp);
+    fputs(comma, fp);
+    fputs(torque, fp);
     fputs("\n", fp);
-
-    // Always remember to close your files
     fclose(fp);
-    // while(n<1000){
-    //    //printf("%d\n", CO_OD_RAM.statusWords.motor1);
-    //    tfgyhj = CO_OD_RAM.statusWords.motor1;
-    //     n++;
-    // }
-    gettimeofday(&stop, NULL);
-    double elapsed_ms = (stop.tv_sec - start.tv_sec) * 1000.0;
-    elapsed_ms += (stop.tv_usec - start.tv_usec) / 1000.0;
-    printf("TASK 1:  %.2f milliseconds\n", elapsed_ms); 
 }
-
-
-/******************************************************************************/
-void app_program1ms(void){
-
+void fileLogHeader(){
+    FILE *fp;
+    fp = fopen("X2_log.txt", "a");
+    char header1[] = "======================================\n";
+    char header2[] = "X2 exoskeleton torque and position log\n";
+    char header3[] = "======================================\n";
+    char header4[]= "LHPos, LHT, LKPos, LKT, RHPos, RHT,RKPos, RKT\n";
+    fputs(header1, fp);
+    fputs(header2, fp);
+    fputs(header3, fp);
+    fputs(header4, fp);
+    fclose(fp);
 }
