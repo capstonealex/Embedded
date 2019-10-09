916533f7d83ffdbb841287878cbd70b44dea49ff
Will campbell <w.campbell@student.unimelb.edu.au>
2019-10-09 15:50:04 +1100

    Fully functional sit stand and walk 5 step app

--- canOpenBeagle/Robogals/X2InitHardwareRoboGals.sh
+++ canOpenBeagle/Robogals/X2InitHardwareRoboGals.sh
@@ -11,12 +11,6 @@ config-pin -a p8.10 in+
 #setting up CAN1 interface on BBB.
 sudo config-pin p9.24 can
 sudo config-pin p9.26 can
-dmesg|grep can
-sudo modprobe can
-sudo modprobe can-dev
-sudo modprobe can-raw
-lsmod|grep can
-mesg|grep raw
 sudo ip link set can1 up type can bitrate 1000000
 sudo ifconfig can1 up
 
@@ -25,6 +19,6 @@ sudo ifconfig can1 up
 cd /home/debian/CANopenSocket/canopend
 
 #Setting up master node
-app/canopend can1 -i 100 -c ""
+app/canopend can1 -i 100 -c ""  &
 
 
