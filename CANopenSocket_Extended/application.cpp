e22bd69dbd0deda5f0804eee93d6c1a9bf0eb32f
Mihai Blaga <48316682+Mihai-Blaga@users.noreply.github.com>
2019-10-24 23:36:41 +1100

    Logging changes
    Redid the logging code to neaten it up but also introduced different logging system using spdlog.
    Have not checked the code for errors as unsure of how to do this as of yet.

--- CANopenSocket_Extended/application.cpp
+++ CANopenSocket_Extended/application.cpp
@@ -28,12 +28,26 @@
 #include <stdint.h>
 #include <sys/time.h>
 
+//header files for the implementing logging using spdlog techniques.
+#include <iostream>
+#include <string.h>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
 //// Data logger helper functions
 void fileLoggerBinary();
 void fileLogHeader();
 void fileLogger();
 void strreverse(char *begin, char *end);
 void itoa(int value, char *str, int base);
+
+/* File has been modified by Mihai Blaga on 24/10 to implement more robust
+ * logging through the implementation of spdlog.
+ */
+auto* createLogger(std::string logID, std::string fileLocation);
+//Path to the file in which all of the log files are stored.
+const std::string logFolder = "\logs\";
+
 /******************************************************************************/
 void app_programStart(void){
     //void fileLogHeader();
@@ -65,8 +79,31 @@ printf("time after(s): %lu, (us): %lu\n",tv.tv_sec, tv.tv_usec);
 }
 
 /******************************************************************************/
+//creating a logger at a designated fileLocation.
+auto* createLogger(std::string logID, std::string fileLocation){
+	try{
+	 	auto logger = spdlog::basic_logger_mt(logID, fileLocation);
+		setLoggerStyle(logger);
+		return logger;
+	} catch (const spdlog::spdlog_ex& ex){
+		std::cout << "Failed to create log: " << ex.what() << std::endl;
+		return;
+	}
+}
+
+//setting the style of the logger to only hold the data without any extra information.
+void setLoggerStyle(auto* logger){
+	logger->set_pattern("%v");	
+}
+/******************************************************************************/
+
+/******************************************************************************/
+//treated as main function.
 void app_program1ms(void){
-	fileLoggerBinary();
+	mainLogger = createLogger("parent", logFolder + "X2_log.txt");
+	spd::set_default_logger(mainLogger);
+	
+	fileLoggerBinary(mainLogger);
 }
 /******************************************************************************/
 void itoa(int value, char *str, int base)
@@ -101,106 +138,103 @@ void strreverse(char *begin, char *end)
         aux = *end, *end-- = *begin, *begin++ = aux;
 }
 /******************************************************************************/
-void fileLogger(){
+void fileLogger(auto* logger){
     //printf("fileLogger beggining\n");
-    FILE* fp;
-    fp = fopen("X2_log.txt", "a");
+	
     // Generate whatever you want logged here, "data" is just an example
-    char position [50];
+    	char position [50];
 	char timestamp [50];
-    char torque[50];
-    char comma[] = ", ";
+    	char torque[50];
+    	char comma[] = ", ";
 	
 	//Getting timestamp
 	//printf("time(s): %lu, (us): %lu\n",tv.tv_sec, tv.tv_usec);
-    //itoa(timer1msDiff, position, 10);
+    	//itoa(timer1msDiff, position, 10);
 	struct timeval tv;
 	gettimeofday(&tv,NULL);
 	itoa(tv.tv_sec, timestamp, 10);
-    fputs(timestamp, fp);
-    fputs(comma, fp);
+	
+	mainLogger->info("{}, ", timestamp);
+	
 	itoa(tv.tv_usec, timestamp, 10);
-	fputs(timestamp, fp);
-    fputs(comma, fp);
 	
-    // Motor 1: Left Hip position and Torque
-    itoa(CO_OD_RAM.actualMotorPositions.motor1, position, 10);
-    itoa(((int16_t)CO_OD_RAM.statusWords.motor1), torque, 10);
-    fputs(position, fp);
-    fputs(comma, fp);
-    fputs(torque, fp);
-    fputs(comma, fp);
-    // Motor 2: Left Knee position and Torque
-    itoa(CO_OD_RAM.actualMotorPositions.motor2, position, 10);
-    itoa(((int16_t)CO_OD_RAM.statusWords.motor2), torque, 10);
-    fputs(position, fp);
-    fputs(comma, fp);
-    fputs(torque, fp);
-    fputs(comma, fp);
-    // Motor 3: Right Hip position and Torque
-    itoa(CO_OD_RAM.actualMotorPositions.motor3, position, 10);
-    itoa(((int16_t)CO_OD_RAM.statusWords.motor3), torque, 10);
-    fputs(position, fp);
-    fputs(comma, fp);
-    fputs(torque, fp);
-    fputs(comma, fp);
-    // Motor 4: Right Knee position and Torque
-    itoa(CO_OD_RAM.actualMotorPositions.motor4, position, 10);
-    itoa(((int16_t)CO_OD_RAM.statusWords.motor4), torque, 10);
-    fputs(position, fp);
-    fputs(comma, fp);
-    fputs(torque, fp);
-    fputs("\n", fp);
+	mainLogger->info("{}, ", timestamp);
 	
-    fclose(fp);
+	uint32_t motorpos[4];
+	uint16_t motorTor[4];
+	
+	motorpos[0] = CO_OD_RAM.actualMotorPositions.motor1;
+	motorpos[1] = CO_OD_RAM.actualMotorPositions.motor2;
+	motorpos[2] = CO_OD_RAM.actualMotorPositions.motor3;
+	motorpos[3] = CO_OD_RAM.actualMotorPositions.motor4;
+	
+	motorTor[0] = CO_OD_RAM.statusWords.motor1;
+	motorTor[1] = CO_OD_RAM.statusWords.motor2;
+	motorTor[2] = CO_OD_RAM.statusWords.motor3;
+	motorTor[3] = CO_OD_RAM.statusWords.motor4;
+	
+	/* Motor 1: Left Hip
+	 * Motor 2: Left Knee
+	 * Motor 3: Right Hip
+	 * Motor 4: Right Knee
+	 */
+	
+	std::stringstream output;
+	
+	for (int i = 0; i<4; i++){
+		itoa(motorpos[i], position, 10);
+		itoa(motorTor[i], torque, 10);
+		
+		output << position << ", " << torque << ", ";
+	}
+	
+	output << "\n";
+	
+	logger->info(output.str());
 	
 }
-void fileLogHeader(){
-    FILE *fp;
-    fp = fopen("X2_log.txt", "a");
-    char header1[] = "======================================\n";
-    char header2[] = "X2 exoskeleton torque and position log\n";
-    char header3[] = "======================================\n";
-    char header4[]= "Time(s), time(ms) LHPos, LHT, LKPos, LKT, RHPos, RHT,RKPos, RKT\n";
-    fputs(header1, fp);
-    fputs(header2, fp);
-    fputs(header3, fp);
-    fputs(header4, fp);
-    fclose(fp);
+void fileLogHeader(auto* logger){
+   	mainLogger = createLogger("parent", logFolder + "X2_log.txt");
+	spd::set_default_logger(mainLogger);
+	
+	std::stringstream header;
+	
+    	header << "======================================\n";
+    	header << "X2 exoskeleton torque and position log\n";
+    	header << "======================================\n";
+    	header << "Time(s), time(ms) LHPos, LHT, LKPos, LKT, RHPos, RHT, RKPos, RKT\n";
+    
+	logger->info(header.str());
 }
 
-void fileLoggerBinary(){
-    FILE* fp;
-    uint32_t sizeInt = 0;
-    fp = fopen("/media/sdcard1/X2logs/X2_log.bin", "ab");
-	if(fp==NULL)
-		printf("\nFILE CREATION ERROR\n");
+void fileLoggerBinary(auto* logger){
+    	mainLogger = createLogger("parent", logFolder + "parent.txt");
+	spd::set_default_logger(mainLogger);
 
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
+    	struct timeval tv;
+    	gettimeofday(&tv,NULL);
 
-    uint32_t motor1pos=CO_OD_RAM.actualMotorPositions.motor1;
-    uint32_t motor2pos=CO_OD_RAM.actualMotorPositions.motor2;
-    uint32_t motor3pos=CO_OD_RAM.actualMotorPositions.motor3;
-    uint32_t motor4pos=CO_OD_RAM.actualMotorPositions.motor4;
-    uint16_t motor1Tor=CO_OD_RAM.statusWords.motor1;
-    uint16_t motor2Tor=CO_OD_RAM.statusWords.motor2;
-    uint16_t motor3Tor=CO_OD_RAM.statusWords.motor3;
-    uint16_t motor4Tor=CO_OD_RAM.statusWords.motor4;
-    long long timesec=tv.tv_sec;
-    long timeusec=tv.tv_usec;
+	uint32_t motorpos[4];
+	uint16_t motorTor[4];
+	
+	motorpos[0] = CO_OD_RAM.actualMotorPositions.motor1;
+	motorpos[1] = CO_OD_RAM.actualMotorPositions.motor2;
+	motorpos[2] = CO_OD_RAM.actualMotorPositions.motor3;
+	motorpos[3] = CO_OD_RAM.actualMotorPositions.motor4;
+	
+	motorTor[0] = CO_OD_RAM.statusWords.motor1;
+	motorTor[1] = CO_OD_RAM.statusWords.motor2;
+	motorTor[2] = CO_OD_RAM.statusWords.motor3;
+	motorTor[3] = CO_OD_RAM.statusWords.motor4;
+	
+    	long long timesec=tv.tv_sec;
+    	long timeusec=tv.tv_usec;
 
-
-    fwrite(&timesec, sizeof(timesec), 1, fp);
-    fwrite(&timeusec, sizeof(timeusec), 1, fp);
-    fwrite(&motor1pos, sizeof(sizeInt), 1, fp);
-    fwrite(&motor1Tor, sizeof(sizeInt), 1, fp);
-    fwrite(&motor2pos, sizeof(sizeInt), 1, fp);
-    fwrite(&motor2Tor, sizeof(sizeInt), 1, fp);
-    fwrite(&motor3pos, sizeof(sizeInt), 1, fp);
-    fwrite(&motor3Tor, sizeof(sizeInt), 1, fp);
-    fwrite(&motor4pos, sizeof(sizeInt), 1, fp);
-    fwrite(&motor4Tor, sizeof(sizeInt), 1, fp);
-
-    fclose(fp);
+	logger->info("{}", timesec);
+	logger->info("{}", timeusec);
+	
+	for(int i = 0; i<4; i++){
+		mainLogger->info("{}", &motorpos[i]);
+		mainLogger->info("{}", &motorTor[i]);
+	}
 }
